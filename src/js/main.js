b93c8a6e8e492f962fcd70ea5850212cd3088970
Vlad Tepes <vg.email.2@gmail.com>
2018-04-03 13:37:43 +0300

    Section 4 added

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,28 @@
- $(document).ready(function(){
-      $('.slider').bxSlider({
-          speed: 1500
-      });
-      svg4everybody();
+$(document).ready(function () {
+    $('.slider').bxSlider({
+        speed: 1500
     });
+    svg4everybody();
+
+    $('#tel').mask('8 (000) 000-00-00');
+
+    $('.tab-bar__link').on('click', function (e) {
+        e.preventDefault();
+
+        var
+            $this = $(this),
+            item = $this.closest('.tab-bar__list-item'),
+            container = $this.closest('.main4__top'),
+            content = container.find('.description__tab-item'),
+            ndx = item.index();
+
+        item.addClass('active')
+            .siblings()
+            .removeClass('active');
+
+        content.eq(ndx)
+            .addClass('active-d')
+            .siblings()
+            .removeClass('active-d');
+    });
+});
